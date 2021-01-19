1120ad38989ae7d8eaf0b11006de2e857b0b5461
mpr812 <68557242+mpr812@users.noreply.github.com>
2021-01-19 16:51:46 +0100

    Update portada.php
    Actualizando portada

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada actualizada",
+"titulo" => "Portada actualizada MPR812",
 "autor" => "mtorres",
 "resumen" => "Resumen de la portada",
 ];
